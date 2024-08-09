20723dc102488a02539f99b2b923d15c1d2821bf
flyyy26 <rfchannel0@gmail.com>
2024-08-09 14:37:26 +0700

    Lacak Paket

---
+++ pages/lacak-paket.js
@@ -0,0 +1,97 @@
+import styles from '@/styles/Lacak.module.css'
+import banner from '@/styles/Banner.module.css'
+import Link from 'next/link'
+import { CiSearch } from "react-icons/ci";
+import { FaPaperPlane } from "react-icons/fa";
+import { FaInbox } from "react-icons/fa";
+import { FaTruck } from "react-icons/fa";
+import { FaHistory } from "react-icons/fa";
+
+export default function LacakPaket(){
+    return(
+        <>
+        <div className={styles.bg_page}>
+            <div className={banner.banner}>
+                <img src='images/banner_lacak.png' alt='Lacak Paket HiXpress'/>
+            </div>
+            <div className={banner.breadcumbs}>
+                <p><Link href='/'>Beranda</Link> / <span>Lacak Paket</span></p>
+            </div>
+            <div className={styles.section1}>
+                <img src='images/lacak_icon_1.png' className={styles.lacak_icon_1}/>
+                <img src='images/lacak_icon_2.png' className={styles.lacak_icon_2}/>
+                <div className={styles.section1_box}>
+                    <h1>Pelacakan Paket</h1>
+                    <p>Silakan masukkan nomor resi Anda pada formulir berikut</p>
+                    <form>
+                        <div className={styles.search_box}>
+                            <input type='text'/>
+                            <CiSearch/>
+                        </div>
+                        <button>Lacak Sekarang</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+        <div className={styles.popup_lacak}>
+            <div className={styles.overlay}></div>
+            <div className={styles.popup_box}>
+                <div className={styles.popup_heading}>
+                    <FaPaperPlane/>
+                    <h3>HIX20240801UN8LVCT27</h3>
+                </div>
+                <div className={styles.from_to}>
+                    <div className={styles.from_to_icon}>
+                        <FaInbox/>
+                        <span>From</span>
+                    </div>
+                    <div className={styles.from_to_address}>
+                        <h3>KEC. PURBARATU</h3>
+                    </div>
+                </div>
+                <div className={styles.from_to}>
+                    <div className={styles.from_to_icon}>
+                        <FaTruck/>
+                        <span>To</span>
+                    </div>
+                    <div className={styles.from_to_address}>
+                        <h3>KEC. TUGUJAYA</h3>
+                    </div>
+                </div>
+                <div className={styles.shipping_status}>
+                    <div className={styles.popup_heading}>
+                        <FaHistory/>
+                        <h3>Status Pengiriman</h3>
+                    </div>
+                    <div className={styles.shipping_status_process}>
+                        <div className={styles.shipping_status_box}>
+                            <h5>PENJUAL TELAH MENGATUR PENGIRIMAN</h5>
+                            <span>01-08-2024 - 21:27</span>
+                        </div>
+                        <div className={styles.shipping_status_box}>
+                            <h5>PESANAN AKAN DIAMBIL OLEH DRIVER</h5>
+                            <span>01-08-2024 - 21:45</span>
+                        </div>
+                        <div className={styles.shipping_status_box}>
+                            <h5>PESANAN SEDANG DIAMBIL OLEH DRIVER</h5>
+                            <span>02-08-2024 - 14:13</span>
+                        </div>
+                        <div className={styles.shipping_status_box}>
+                            <h5>PESANAN TELAH BERHASIL DIAMBIL</h5>
+                            <span>02-08-2024 - 14:19</span>
+                        </div>
+                        <div className={styles.shipping_status_box}>
+                            <h5>PESANAN SEDANG DI ANTAR OLEH DRIVER</h5>
+                            <span>02-08-2024 - 15:17</span>
+                        </div>
+                        <div className={styles.shipping_status_box}>
+                            <h5>PESANAN TELAH SAMPAI</h5>
+                            <span>02-08-2024 - 16:57</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        </>
+    )
+}
